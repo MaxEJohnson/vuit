7bc09b601ad35cd261d46624eb996df99623e057
Max Johnson <mjohnsmax@gmail.com>
2025-03-25 07:25:02 -0700

    Add deb package

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 ### (V)im (U)ser (I)nterface (T)erminal
 
 A buffer manager layer for Vim that provides a terminal-like interface to search for, open, and edit files.
+
+## Installation
+
+### Mac
+
+```bash
+brew tap MaxEJohnson/vuit
+brew install vuit
+```
+
+### Linux
+
+Install .deb package from realeases page.
+
+```bash
+sudo dpkg -i vuit_0.1.0_amd64.deb
+```
--- vuit.rb
+++ vuit.rb
@@ -2,7 +2,7 @@ class vuit < Formula
   desc "VUIT: Vim User Interface Terminal"
   homepage "https://github.com/MaxEJohnson/vuit"
   url "https://github.com/MaxEJohnson/vuit/releases/download/vuit-v0.1.0.tar.gz"
-  sha256 "cdeb8ff5ead50d96841447eb68ab0c55eeba98d012f4e785804cf33950301ba6"
+  sha256 "170b2280a3823cb416a3323c4df14b4c37b02991a1d92fe2792281a5828e478d"
   version "0.1.0"
 
   depends_on "vim"
