3a599f9974d3b85b3c5da6f17ee523bd3bc03348
MaxEJohnson <mjohnsmax@gmail.com>
2025-03-26 23:10:26 -0700

    Update installer for split install

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@
 
 GREEN='\033[0;32m'
 NC='\033[0m'
-VERSION=0.1.0
+MAC_VERSION=0.1.1
+LINUX_VERSION=0.1.0
+
 
 echo -e "${NC}Installing fd and fzf${NC}"
 if [[ $OSTYPE == linux-* ]]; then
@@ -19,16 +21,16 @@ echo -e "${GREEN}Dependencies installed${NC}"
 
 echo -e "${NC}Installing vuit${NC}"
 if [[ $OSTYPE == linux-* ]]; then
-    curl -L -O https://github.com/MaxEJohnson/vuit/releases/download/v$VERSION/vuit_${VERSION}_amd64.deb
-    sudo dpkg -i vuit_${VERSION}_amd64.deb
+    curl -L -O https://github.com/MaxEJohnson/vuit/releases/download/v${LINUX_VERSION}/vuit_${LINUX_VERSION}_amd64.deb
+    sudo dpkg -i vuit_${LINUX_VERSION}_amd64.deb
 else
-    curl -L -O https://github.com/MaxEJohnson/vuit/releases/download/v$VERSION/vuit-v$VERSION-macos-arm64.tar.gz
-    tar -xvf vuit-v$VERSION-macos-arm64.tar.gz
+    curl -L -O https://github.com/MaxEJohnson/vuit/releases/download/v${MAC_VERSION}/vuit-v${MAC_VERSION}-macos-arm64.tar.gz
+    tar -xvf vuit-v${MAC_VERSION}-macos-arm64.tar.gz
     sudo mkdir -p /usr/local/bin
     sudo mv vuit /usr/local/bin/vuit
     sudo chmod +x /usr/local/bin/vuit
     sudo xattr -d com.apple.quarantine /usr/local/bin/vuit 2> /dev/null
-    rm vuit-v$VERSION-macos-arm64.tar.gz
+    rm vuit-v${MAC_VERSION}-macos-arm64.tar.gz
 fi
 
 echo -e "${GREEN}vuit installed${NC}"
