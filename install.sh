e2df66982c3e505e9d4a3d1eaef60e14e7e6c382
MaxEJohnson <mjohnsmax@gmail.com>
2025-04-12 11:12:54 -0700

    Update to v0.7.0

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 
 GREEN='\033[0;32m'
 NC='\033[0m'
-MAC_VERSION=0.6.5
-LINUX_VERSION=0.6.5
+MAC_VERSION=0.7.0
+LINUX_VERSION=0.7.0
 
 echo -e "${NC}Installing vuit${NC}"
 if [[ $OSTYPE == linux-* ]]; then
